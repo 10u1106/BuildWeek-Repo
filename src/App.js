058e427ba45994f1c072f5c2bc755d0c5170f4f2
Saraswat_Tanu <tanusaraswat26@gmail.com>
2021-04-19 21:48:29 +0530

    changes made...

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>Let's begin the build-week!!</h2>
+      <h2>Let's begin the build-week!</h2>
     </div>
   );
 }
