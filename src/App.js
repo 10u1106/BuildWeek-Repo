a85ec1e0cf5b89d257dc2f5b371b40ca32b6ddc5
Saraswat_Tanu <tanusaraswat26@gmail.com>
2021-04-19 22:49:32 +0530

    some changes done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
+  const [searchText, setSearchSet] = useState("");
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Let's begin the build-week!</h2>
+      <input
+        type="text"
+        name="search"
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+      />
     </div>
   );
 }
